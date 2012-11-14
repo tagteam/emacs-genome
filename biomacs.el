cdf1099128afbd8d67a8b6a942457c958b0be208
tagteam <tag@biostat.ku.dk>
2012-11-14 13:52:43 +0100

    added anything

--- biomacs.el
+++ biomacs.el
@@ -1,18 +1,20 @@
 (add-to-list 'load-path (concat (getenv "HOME") "/emacs-genome/snps/"))
 (add-to-list 'load-path (concat (getenv "HOME") "/emacs-genome/genes/"))
-(load (concat (getenv "HOME") "/emacs-genome/genes/ess/lisp/ess-site"))
 ;; utility functions
 (require 'eg-utility-snps)
 ;; buffer cycling
 (require 'cycle-buffer-snps)
 ;; Emacs speaks statistics
 (setq ess-etc-directory-list nil)
-(load (concat (getenv "HOME") "/emacs-genome/genes/ess/lisp/ess-site"))
+(load (concat (getenv "HOME") "/emacs-genome/genes/ESS/lisp/ess-site"))
 (require 'ess-R-snps)
 (require 'ess-edit)
 ;; LaTeX
 (require 'latex-snps)
 ;; keybindings
 (require 'key-snps)
+;; anything-recoll
+(require 'anything)
+(require 'anything-recoll-snps)
 
 
