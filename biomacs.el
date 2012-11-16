c4cb669c93f7096c6eb505e973060afa4b16b7a4
tagteam <tag@biostat.ku.dk>
2012-11-16 14:55:35 +0100

    fixed typo

--- biomacs.el
+++ biomacs.el
@@ -1,15 +1,18 @@
-(setq emacs-novicet)
+(setq emacs-novice t)
 (add-to-list 'load-path (concat (getenv "HOME") "/emacs-genome/snps/"))
 (add-to-list 'load-path (concat (getenv "HOME") "/emacs-genome/genes/"))
 ;; general purpose utility functions
 (require 'eg-utility-snps nil t)
+;; anything
+(require 'anything nil t)
+(require 'anything-recoll-snps nil t)
 ;; completion
 (require 'hippie-exp nil t)
 ;; buffer cycling
 (require 'iswitchb nil t)
-(iswitchb-mode nil t)
+(iswitchb-mode t)
 (setq iswitchb-default-method 'samewindow)
-(setq iswitchb-case nil t)
+(setq iswitchb-case t)
 (require 'cycle-buffer-snps nil t)
 ;; Emacs speaks statistics
 (setq ess-etc-directory-list nil)
@@ -20,9 +23,6 @@
 (require 'latex-snps nil t)
 ;; keybindings
 (require 'key-snps nil t)
-;; anything-recoll
-(require 'anything nil t)
-(require 'anything-recoll-snps nil t)
 ;; folding
 (require 'folding-snps nil t)
 
