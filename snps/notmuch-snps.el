7f1fd765b871b0ac94835315429b3d821dbb0f5e
Thomas Alexander Gerds <tag@biostat.ku.dk>
2021-02-11 06:44:17 +0100

    added opgave.el

---
+++ snps/notmuch-snps.el
@@ -0,0 +1,60 @@
+;; first time usage needs: notmuch setup
+(setq notmuch-show-elide-non-matching-messages t)
+(setq notmuch-show-only-matching-messages t)
+
+;;{{{ search-help
+(require 'notmuch)
+(require 'notmuch-lib)
+(setq notmuch-hello-sections
+      (list #'notmuch-hello-insert-help
+	    #'notmuch-hello-insert-saved-searches
+	    #'notmuch-hello-insert-search
+	    #'notmuch-hello-insert-recent-searches
+	    #'notmuch-hello-insert-alltags
+	    #'notmuch-hello-insert-footer))
+
+(defun notmuch-hello-insert-help ()
+  (widget-insert "from:word \t match messages with word in the from address field.\n")
+  (widget-insert "to:word match \t messages with word in either the To: or Cc: headers.\n")
+  (widget-insert "attachment:word\t match messages with word in an attachment filename.\n")
+  (widget-insert "subject:word\t match messages with word in the subject field.\n"))
+
+;;}}}
+;;{{{ Gnus
+
+;; adapted from http://www.emacswiki.org/emacs/NotMuch
+
+(setq notmuch-fcc-dirs nil)
+(defun notmuch-file-to-group (file)
+  "Calculate the Gnus group name from the given file name."
+  (let ((group (directory-file-name (file-name-directory file))))
+    (concat "nnml:"
+	    (replace-regexp-in-string "/" "."
+				      (replace-regexp-in-string "^/" "" (replace-regexp-in-string (expand-file-name "~/mail") "" group))))))
+
+
+(defun notmuch-goto-message-in-gnus ()
+  "Open a summary buffer containing the current notmuch
+     article."
+  (interactive)
+  (unless (gnus-alive-p) (with-temp-buffer (gnus)))
+  (let ((group (notmuch-file-to-group (notmuch-show-get-filename)))
+     	(message-id
+     	 (replace-regexp-in-string "\"" ""
+				   (replace-regexp-in-string "^id:" ""
+							     (notmuch-show-get-message-id)))))
+    (if (and group message-id)
+	(org-gnus-follow-link group message-id)
+      (message "Couldn't get relevant infos for switching to Gnus."))))
+;; (progn
+;; (gnus-summary-read-group group 1) ; have to show at least one old message
+;; (gnus-summary-refer-article message-id)) ; simpler than org-gnus method?
+;; (message "Couldn't get relevant infos for switching to Gnus."))))
+
+(define-key notmuch-show-mode-map (kbd "C-c C-c") 'notmuch-goto-message-in-gnus)
+(define-key notmuch-search-mode-map " " 'notmuch-goto-message-in-gnus)
+;;}}}
+
+
+(provide 'notmuch-snps)
+
---
+++ snps/org-opgave-snps.el
@@ -0,0 +1,116 @@
+;;; org-opgave-snps.el --- export org to Rmd for teaching exercises  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Thomas Alexander Gerds
+
+;; Author: Thomas Alexander Gerds <tag@biostat.ku.dk>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+
+(provide 'org-opgave-snps)
+
+(add-to-list 'load-path (expand-file-name "genes/orgmode-accessories/" emacs-genome))
+(require 'ox-extra)
+(require 'ox-md)
+(require 'ox-ravel)
+(require 'superman)
+
+(defun never-plain-export ()
+  (if (memq org-export-current-backend '(html latex docx))
+      "no" "yes"))
+(add-to-list 'superman-org-export-target-list "opgave")
+
+(defun exercise-with-code ()
+  (if exercise-without-code "both" "none"))
+
+(defun exercise-with-solutions ()
+  (if exercise-with-solutions "both" "none"))
+
+(defun superman-export-as-opgave (&optional arg)
+  "Export orgmode R-exercise to 3 different targets:
+   Rmd without code
+   Rmd with code
+   html with solutions
+
+here is how to mark the solution sections
+
+*** Solution :solution:ignore:
+
+and similarly the rdm only sections
+
+*** rmd section :rmd:ignore:
+
+there are two blocks:
+
+#+BEGIN_SRC R  :results output :exports (exercise-without-code) :cache yes :eval (never-plain-export)
+  # put your R-code here 
+#+END_SRC
+
+#+BEGIN_SRC R  :results output :exports (exercise-with-code)  :session *R* :cache yes  :eval (never-plain-export)
+library(data.table)
+space <- fread('data/space.csv')
+space
+#+END_SRC
+"
+  (interactive)
+  (let* ((org-buf (current-buffer))
+	 (rmd-file (org-export-output-file-name ".Rmd"))
+	 (rmd-code-file (concat (file-name-sans-extension rmd-file) "-with-code.Rmd"))
+	 (rmd-solution-file (concat (file-name-sans-extension rmd-file) "-with-solution.Rmd"))
+	 (org-export-exclude-tags (list "noexport"))
+	 (org-export-with-toc t)
+	 (exercise-with-code nil)
+	 (exercise-with-solutions nil))
+    ;; 
+    ;; export to Rmd with code without toc (first without solutions)
+    ;;
+    (save-buffer)
+    ;; to avoid pop-up 
+    ;; (save-window-excursion
+    ;; (find-file rmd-file)
+    ;; (kill-buffer))
+    (setq exercise-with-code t  ;; evaluate blocks/chunks when :exports (exercise-with-code)
+	  org-export-exclude-tags (list "noexport" "solution") ;; ignore these sections
+	  org-export-with-toc nil)
+    (Rmd-export)
+    (copy-file rmd-file (concat (file-name-sans-extension rmd-file) "-with-code.Rmd") 'ok)
+    (setq exercise-with-code nil)
+    ;; 
+    ;; export to Rmd with code without toc (now with solutions)
+    ;;
+    (setq exercise-with-solutions t ;; evaluate 
+	  org-export-exclude-tags (list "noexport" "instruks");; now include solution sections
+	  org-export-with-toc nil)
+    (Rmd-export)
+    (copy-file rmd-file (concat (file-name-sans-extension rmd-file) "-with-solution.Rmd") 'ok)
+    (setq exercise-without-code nil)
+    (save-buffer)
+    ;; revert rmd buffer
+    (find-file (concat (file-name-sans-extension rmd-file) "-with-solution.Rmd"))
+    (revert-buffer t t t)
+    (find-file (concat (file-name-sans-extension rmd-file) "-with-code.Rmd"))
+    (revert-buffer t t t)
+    (copy-file rmd-code-file "~/Desktop/Demografi/" 'yes 'yes)
+    (copy-file rmd-solution-file "~/Desktop/Demografi/" 'yes 'yes)
+    (superman-set-config (concat (buffer-name org-buf) " | " rmd-code-file " / " rmd-solution-file))))
+
+;;; org-opgave-snps.el ends here
